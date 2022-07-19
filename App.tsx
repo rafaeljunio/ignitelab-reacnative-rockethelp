10317fb7929df04d7700cbe295cc9fcb0b93df34
Rafael Souza <rafael.souza@iexfy.com>
2022-07-18 21:51:26 -0300

    finalização da tela de login

--- App.tsx
+++ App.tsx
@@ -1,22 +1,26 @@
-import { NativeBaseProvider, Box } from 'native-base';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 
+import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
+import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 2,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
